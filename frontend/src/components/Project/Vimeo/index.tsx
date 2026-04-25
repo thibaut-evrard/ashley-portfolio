3f3a12e863fde777c5c9d3cb0caee2c355cefc03
thibaut evrard <thibaut.evrard.1509@gmail.com>
2026-04-25 13:31:08 +0100

    Add basic vimeo style

--- frontend/src/components/Project/Vimeo/index.tsx
+++ frontend/src/components/Project/Vimeo/index.tsx
@@ -1,11 +1,13 @@
+import "./styles.scss";
+
 interface Props {
   content: { url: string };
 }
 
 export default function ProjectVimeo(props: Props) {
-  console.log("Rendering Vimeo with URL:", props.content.url);
   return (
     <iframe
+      class="project-vimeo"
       title="Vimeo Video"
       src={props.content.url}
       allow="autoplay; fullscreen; picture-in-picture"
