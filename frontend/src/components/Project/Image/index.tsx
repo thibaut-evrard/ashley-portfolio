d837dbe01d037025623d22e2b226086272a76b80
thibaut evrard <thibaut.evrard.1509@gmail.com>
2026-04-25 13:22:45 +0100

    Fix image src

--- frontend/src/components/Project/Image/index.tsx
+++ frontend/src/components/Project/Image/index.tsx
@@ -1,9 +1,12 @@
 import { MediaImageContent } from "@/content/types";
+import { getPublicMedia } from "@/utils/public";
 
 interface Props {
   content: MediaImageContent;
 }
 
 export default function ProjectImage(props: Props) {
-  return <img src={props.content.src} alt={props.content.alt} />;
+  return (
+    <img src={getPublicMedia(props.content.src)} alt={props.content.alt} />
+  );
 }
